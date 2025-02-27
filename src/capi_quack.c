6e0f91f6befb3655c79bcfdc649f6efa8aa21ce4
Mathias Lafeldt <mathias.lafeldt@gmail.com>
2025-02-27 11:51:45 +0100

    Build extension with Zig

--- src/capi_quack.c
+++ src/capi_quack.c
@@ -2,10 +2,19 @@
 
 #include "add_numbers.h"
 
+// Workaround for missing struct tag in DUCKDB_EXTENSION_ENTRYPOINT (DuckDB 1.1.x)
+typedef struct duckdb_extension_access duckdb_extension_access;
+
+#if DUCKDB_EXTENSION_API_VERSION_MAJOR >= 1
+#define EXTENSION_RETURN(result) return (result)
+#else
+#define EXTENSION_RETURN(result) return
+#endif
+
 DUCKDB_EXTENSION_ENTRYPOINT(duckdb_connection connection, duckdb_extension_info info, struct duckdb_extension_access *access) {
 	// Register a demo function
 	RegisterAddNumbersFunction(connection);
 
 	// Return true to indicate succesful initialization
-	return true;
+	EXTENSION_RETURN(true);
 }
